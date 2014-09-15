829a459f90dcea7f83afb0880b34f4b93bafb7ea
twneale <twneale@gmail.com>
2014-09-14 22:46:11 -0400

    Packaging madness

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ VERSION = '0'
 long_description = ''
 
 setup(name='banzai',
+      py_modules=['banzai'],
       version=VERSION,
       packages=find_packages(exclude="tests"),
       author='Thom Neale',

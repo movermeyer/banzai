ad956ebeeb7bded62d6e56e09d7187639feacbbe
twneale <twneale@gmail.com>
2014-09-19 23:46:21 -0400

    Added spider

--- banzai/io.py
+++ banzai/io.py
@@ -1,5 +1,11 @@
 import os
 import re
+import collections
+import urllib.parse
+
+import requests
+import lxml.html
+
 from hercules import CachedAttr
 
 import banzai
@@ -75,7 +81,7 @@ def regex_filter(*, exclude=None, include=None, rgx_method='search'):
         exclude=exclude, include=include, rgx_method=rgx_method)
 
 
-def find_files(start_dir, include=None, exclude=None, rgx_method='search', match_abspath=True):
+def find_files(start_dir, *, include=None, exclude=None, rgx_method='search', match_abspath=True):
     '''Walks the filesystem starting at start_dir and yields absolute
     filenames. If include or exclude is specified, applies them via a
     regex_filter.
@@ -98,3 +104,110 @@ def find_files(start_dir, include=None, exclude=None, rgx_method='search', match
                     yield path
             else:
                 yield path
+
+
+class Url:
+    '''A url object that can be compared with other url orbjects
+    without regard to the vagaries of casing, encoding, escaping,
+    and ordering of parameters in query strings.'''
+    def __init__(self, url):
+        self.url = url
+
+    @CachedAttr
+    def parts(self):
+        parts = urllib.parse.urlparse(self.url)
+        _query = dict(urllib.parse.parse_qsl(parts.query))
+        # Nuke the old path.
+        _query.pop('oldPath', None)
+        _query = frozenset(_query.items())
+        parts = parts._replace(
+            query=_query,
+            path=urllib.parse.unquote_plus(parts.path))
+        return parts
+
+    def __eq__(self, other):
+        return self.parts == other.parts
+
+    def __hash__(self):
+        return hash(self.parts)
+
+    def __repr__(self):
+        return 'Url(%r)' % str(self)
+
+    def __contains__(self, value):
+        return value in str(self)
+
+    def __str__(self):
+        parts = self.parts
+        query = urllib.parse.urlencode(sorted(parts.query))
+        parts = parts._replace(
+            params='',
+            query=query,
+            scheme='http',
+            netloc='www.gpo.gov')
+        return urllib.parse.urlunparse(parts)
+
+
+class UrlFinder:
+    '''Dumb web spider for spidering a site with GET'able urls.
+    '''
+    def __init__(self, start_url):
+        self.start_url = Url(start_url)
+        self.urls = collections.deque()
+        self.processed = set()
+        self.added = set()
+
+    def __iter__(self):
+        self.urls.append(self.start_url)
+        while True:
+            try:
+                url = self.urls.pop()
+            except IndexError:
+                return
+            if url in self.processed:
+                continue
+            for url in self.process_url(url):
+                if self.should_yield(url):
+                    yield url
+
+    def should_yield(self, url):
+        return True
+
+    def get_lxml_doc(self, url):
+        resp = self.get_resp(url)
+        doc = lxml.html.fromstring(resp.content)
+        doc.make_links_absolute(url)
+        return doc
+
+    def get_resp(self, url):
+        '''Dumb, overridable method for fetching urls.
+        '''
+        resp = requests.get(url)
+        return resp
+
+    def already_processed(self, url):
+        return Url(url) in self.processed
+
+    def already_added(self, url):
+        return Url(url) in self.added
+
+    def gen_urls(self, url):
+        doc = self.get_lxml_doc(url)
+        yield from doc.xpath('//a@href')
+
+    def should_follow(self, url):
+        return True
+
+    def process_url(self, url):
+        self.added.add(url)
+        for child_url in self.gen_urls(str(url)):
+            child_url = Url(child_url)
+            if self.should_follow(child_url):
+                self.urls.append(child_url)
+                self.added.add(child_url)
+            yield child_url
+        self.processed.add(url)
+
+# def find_urls(start_url, requests_client):
+#     import lxml.html
+
